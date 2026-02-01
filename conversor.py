e4451f4b7f0b4fc72008b8b44cdd422d18db6835
Wilmer Murillo <womurillo@uth.hn>
2026-01-31 21:38:58 -0600

    Add files via upload

---
+++ conversor.py
@@ -0,0 +1,9 @@
+import streamlit as st
+
+st.title("Este es un conversor de Lempiras a Dolares")
+
+lempiras=st.number_input('Ingrese la cantidad de Lempiras', min_value=0.0)
+
+if st.button ('Proceasr'):
+    dolares=lempiras/26.50
+    st.write(f"El equivalente en dolares es : ${dolares:.2f}")
